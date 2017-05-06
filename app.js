ca842645410eb8c00a14d486fb544d809649803e
soundappans <soundappans@gmail.com>
2017-05-05 20:20:07 -0400

    Proc file added for Heroku app deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function(request,response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 var server=app.listen(port, function(req,res) {
     console.log('Catch the action at local system using http://localhost:' + port );
